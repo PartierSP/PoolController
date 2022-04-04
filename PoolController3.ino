aedfe9129ad047062a5ac4268c160d28e505e970
PartierSP <mikevl1973@gmail.com>
2022-04-03 21:36:20 -0400

    Separated constants and html strings into separate files

--- PoolController3.ino
+++ PoolController3.ino
@@ -3,6 +3,8 @@
 #include <ESP8266WiFi.h>
 #include <Wire.h>
 #include "config.h"
+#include "constants.h"
+#include "html.h"
 
 //The config.h file is used to configure the network SSID and password.  It should
 //contain the following two lines (subsituting your SSID name and password for
@@ -11,12 +13,6 @@
 //  #define SSID_NAME "myssid"
 //  #define NET_PASSWD "mypassword"
 //
-
-//const char WiFiPassword[] = "12345678";
-//const char AP_NameChar[] = "LEDControl";
-
-const int MAX_VAL = -20; // define maximum WiFi signal strength (in dBm)
-const int MIN_VAL = -90; // define minimum WiFi signal strength (in dBm)
 
 char ssid[]=SSID_NAME;
 char pass[]=NET_PASSWD;
@@ -33,94 +29,12 @@ byte line;
 bool ManOveride;
 byte ManOvOff;
 
-String header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-String html_1 = R"====(<!DOCTYPE html><html>
-<head>
-  <meta charset='utf-8'>
-  <link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'> 
-  <title>Pool Control</title>
-</head>
-<body class='w3-pale-blue'>
-  <div class='w3-main'>
-    <div class='w3-blue'>
-      <div class='w3-container'>
-        <h1>Pool Control</h1>
-      </div>
-    </div>
-    <div class='w3-container'>
-      <div class='w3-auto'>
-        <h3>Status</h3>
-)====";
-String html_2 = R"====(
-    <div class='w3-panel w3-card-4 w3-white w3-round-large'>
-      <div class='w3-contatiner w3-center w3-padding'>
-        <form method='get'><input type='hidden' value='1' name='MANOVRD'><input type='hidden' value='0' name='na'><input type='submit' value='Manual Overide' class='w3-button w3-blue w3-round-large'></form> - <form method='get'><input type='hidden' value='1' name='BUMP'><input type='hidden' value='0' name='na'><input type='submit' value='Bump' class='w3-button w3-blue w3-round-large'></form> - <form method='get' onsubmit='synctime()'><div id='time'></div><input type='hidden' value='0' name='na'><input type='submit' value='Sync Time' class='w3-button w3-blue w3-round-large'></form>
-        <script>
-          function synctime(){
-            let currentDate=new Date();
-            let cDoW=currentDate.getDay();
-            let cDay=currentDate.getDate();
-            let cMonth=currentDate.getMonth()+1;
-            let cYear=currentDate.getYear()-100;
-            let cHour=currentDate.getHours();
-            let cMins=currentDate.getMinutes();
-            let cSec=currentDate.getSeconds();
-            
-            document.getElementById('time').innerHTML='<input type=hidden name=SETDOW value='
-              +cDoW+'><input type=hidden name=SETDATE value='
-              +cDay+'><input type=hidden name=SETMNTH value='
-              +cMonth+'><input type=hidden name=SETYEAR value='
-              +cYear+'><input type=hidden name=SETHR value='
-              +cHour+'><input type=hidden name=SETMIN value='
-              +cMins+'><input type=hidden name=SETSEC value='
-              +cSec+'>';
-          }
-        </script>
-      </div>
-    </div>
-)====";
-String html_3 = "";
-String html_4 = R"====(
-       <div class='w3-panel w3-card-4 w3-white w3-round-large'>
-          <div class='w3-container w3-center w3-padding'>
-            <a href='index' class='w3-button w3-blue w3-round-large'>Main Page</a> - <a href='confg' class='w3-button w3-blue w3-round-large'>Configuration Page</a> - <a href='sched' class='w3-button w3-blue w3-round-large'>Schedual Page</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</body>
-</html>
-)====";
-String html_5 = R"====(        <div class='w3-panel w3-card-4 w3-white w3-round-large w4-padding w3-center'>
-          <form method='get'><table class='w3-table w3-bordered w3-centered'><tr><th>Day</th><th>Line</th><th colspan=2 class='w3-pale-green'>Time</th><th>Power</th></tr>)====";
-String html_6 = "<td rowspan=4><input type=text value=Weekend disabled class='w3-input w3-border-0'></td>";
-String html_7 = "<td rowspan=4><input type=text value=Weekday disabled class='w3-input w3-border-0'></td>";
-String html_8 = "</table><p><input type=hidden value=0 name=na><input type=submit value='Update Schedule' class='w3-button w3-blue w3-round-large'></p></form></div>";
-
 String html_status = "";
 String curdatetime = "";
 String request = "";
 
-String sDoW[8]={"Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"};
-int Max[3]={23, 55, 4};
-int Step[3]={1,5,1};
-int ModeDesc[5]={0,25,50,75,100};
-
-int Pool_Pin = D4;
-
 byte Program[8][3]; //program schedule.  Rows 0~3=Weekdays, Rows 4~7=Weekends
-                   //                   Col 2=Powerlevel, Col 0=Hour & Col1=Mins for start time.
-
-                   //Schedule: Rows 0~4 Requested power level (0=off, 1=25%, 2=50%, 3=75%, 4=100%)
-                   //          Columns 0~59 Pump status for that minuite
-bool Schedule[5][60]={
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
-
+                    //                   Col 2=Powerlevel, Col 0=Hour & Col1=Mins for start time.
 
 void setup() {
   Serial.begin(115200);
